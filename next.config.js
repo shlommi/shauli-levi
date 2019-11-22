e4f5256ad249fcecdb162e5811d8b6a42f4f6236
shlommi <shlommi@gmail.com>
2019-11-22 15:00:26 +0200

    adding config file

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	exportPathMap: function() {
+	  return {
+	    '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/mapat-haim': { page: '/mapat-haim' },
+        '/yeutz-hachvana': { page: '/yeutz-hachvana' },
+	  }
+	}
+  }
